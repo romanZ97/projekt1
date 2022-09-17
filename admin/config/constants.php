d5c30405c84b3cfacc053afdb96eda73437548fd
Franck Youmbi <junioryvan5@gmail.com>
2022-09-17 13:47:03 +0200

    Verbindung Dashbord mit Startseite

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,15 +1,14 @@
 <?php 
     
     session_start();
-     // Konstanten erstellen, damit die Werte die mehrmal vorkommen nicht verderholt werden müssen.
-     define('URLRACINE', 'http://localhost:8888/projekt1/');
-     define('LOCALHOST', 'localhost');
-     define('DB_USERNAME', 'root');
-     define('DB_PASSWORD', 'root');
-     define('DB_NAME', 'GastroApp'); //Name der Datenbank
+const URLRACINE = 'http://localhost:8888/projekt1/';
+const LOCALHOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root';
+const DB_NAME = 'GastroApp'; //Name der Datenbank
 
-     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(("Keine Verbindung mit der Datenbank möglich: " . $mysqli->connect_error)); //Verbindung mit der DB
-     $db_select = mysqli_select_db($conn, DB_NAME) or die("Keine Datenbank gefunden: " . $mysqli->connect_error); //Auswahl der DB 
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(("Keine Verbindung mit der Datenbank möglich: " . $conn->connect_error)); //Verbindung mit der DB
+$db_select = mysqli_select_db($conn, DB_NAME) or die("Keine Datenbank gefunden: " . $conn->connect_error); //Auswahl der DB
 
 
 ?> 
